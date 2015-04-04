c7b867d3f0c787ec058fb1fb26d4f941a2386276
JJB1980 <j.j.b1980@live.com>
2015-04-04 10:44:53 +1000

    playing with favicon

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+var favicon = require('serve-favicon');
 
+app.use(favicon(__dirname + '/public/images/favicon2.ico'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 var index = require('./routes/index');
